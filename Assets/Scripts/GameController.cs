fef905ff735af755e039e7f8c7b409d0fabee295
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2023-03-24 13:33:58 +1300

    Added basic walls data and debug map

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeConstructor))]           
+
+public class GameController : MonoBehaviour
+{
+    private MazeConstructor constructor;
+
+    void Awake()
+    {
+        constructor = GetComponent<MazeConstructor>();
+    }
+    
+    void Start()
+    {
+    
+    }
+}
